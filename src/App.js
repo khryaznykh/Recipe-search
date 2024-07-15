9ddfaa887e5d56f8e075f07a5005366fab0977a2
khryaznykh <khryaznykh@gmail.com>
2024-07-15 15:39:00 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import video from './food.mp4';
+
+import RecipesToShow from './RecipesToShow';
+
 
 function App() {
+  const myID = "0bba0094";
+  const myKEY = "8397da2996783123e0c2330081a3b227%09";
+  const [myRecipes, setMyRecipes] = useState([]);
+  const [mySearch, setMySearch] = useState("");
+  const [myFinalSearch,setMyFinalSearch] = useState ("avocado");
+
+  useEffect (() => {
+    const fetchMyRecipes = async () => {
+      const response = await fetch (`https://api.edamam.com/api/recipes/v2?type=public&q=${myFinalSearch}&app_id=${myID}&app_key=${myKEY}`);
+      const data = await response.json();
+      setMyRecipes(data.hits);
+    };
+    fetchMyRecipes()
+  }, [myFinalSearch])
+
+  const myRecipeSearch = (e) => {
+    setMySearch(e.target.value)
+  }
+
+  const finalSearch = (e) => {
+    e.preventDefault();
+    setMyFinalSearch(mySearch)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <video autoPlay muted loop>
+        <source src={video} type="video/mp4" />
+      </video>
+      </div>
+      <div className='container'>
+        <h1>Here you can find over 2.3 million recipes</h1>
+      </div>
+      
+      <div>
+        <form className='container' onSubmit={finalSearch}>
+          <input onChange={myRecipeSearch} placeholder='type your ingredients here to start...' type='text' value={mySearch}/>
+          <button className='buttonSearch' onClick={finalSearch}>Search...</button>
+        </form>
+      </div>
+
+      <div className='recipesArranged'>
+        {myRecipes.map ((item, index) =>{
+          return(
+          <RecipesToShow key = {index}
+            name = {item.recipe.label} 
+            cuisine = {item.recipe.cuisineType} 
+            image = {item.recipe.image} 
+            calories = {item.recipe.calories} 
+            digest = {item.recipe.digest} 
+            ingridients = {item.recipe.ingredientLines} />
+        )})
+        }
+      </div>
     </div>
   );
 }
---
+++ src/RecipesToShow.js
@@ -0,0 +1,44 @@
+import arrow from './arrow.png';
+
+function RecipesToShow({name, cuisine, image, calories, digest, ingridients}) {
+    return (
+        <div className="recipe">
+            <div className="container" >
+                <h2>{name}</h2>
+            </div>
+            <hr className='line'/>
+            <div className="container">
+                <p>{cuisine} food</p>
+            </div>
+
+            <div className="container">
+                <img src={image} alt="Food"/>
+            </div>
+            
+            <div className="container">
+                <p>{calories.toFixed()} cal</p>
+            </div>
+            <div className="container">
+                {digest.map ((nutrivalue, index) => {
+                    if (index < 3) {
+                        return(
+                            <div  key={index}>
+                                <p>{nutrivalue.label} {nutrivalue.total.toFixed()}  {nutrivalue.unit}  </p>                         
+                            </div>
+                        )}
+                    })
+                }
+               
+            </div>
+            <hr className='line'/>
+            {ingridients.map ((line, index) => {
+                return (
+                    <ul className="containerarrow" key={index}>
+                        <li><img className="arrow" src={arrow} alt="arrow"/> {line}</li>
+                    </ul>
+                )
+            })}
+        </div>    
+    )
+} 
+export default RecipesToShow;
